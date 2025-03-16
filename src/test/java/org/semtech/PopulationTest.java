d5a849791b79dbd97807f550bea42c9e71f8f530
Jagga Muddasani <saibaba.jagan@gmail.com>
2025-03-16 18:22:43 -0400

    added directory structure

---
+++ src/test/java/org/semtech/PopulationTest.java
@@ -0,0 +1,28 @@
+package org.semtech;
+
+class PopulationTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMaxPopulationByDept() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTotalPopulationByDept() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMinPopulationForAllDepts() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
